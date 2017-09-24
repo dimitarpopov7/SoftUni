66f760a44eb0115b5732d632d2ef1ed0b89ee8f8
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-09-24 21:30:19 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/7. Lists Exercises/04. Sum Reversed Num/04. Sum Reversed Num/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Sum_Reversed_Num
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<string> reversedNums = new List<string>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string reverse = new string(numbers[i].ToString().Reverse().ToArray());
+                reversedNums.Add(reverse);
+            }
+            List<int> sumList = new List<int>();
+            for (int i = 0; i < reversedNums.Count; i++)
+            {
+                sumList.Add(Convert.ToInt32(reversedNums[i]));
+            }
+            Console.WriteLine(sumList.Sum());
+        }
+    }
+}
