ea42d8321affb45db8a4a9c67819785c3a3d1eb5
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-09-25 14:10:51 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/7. Lists Exercises/05. Bomb Number 2/05. Bomb Number 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Bomb_Number_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] bomb = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int specialN = bomb[0]; int power = bomb[1];
+            while (nums.Contains(specialN))
+            {
+                int indexSpecialN = nums.IndexOf(specialN);
+                for (int num = indexSpecialN - power; num <= indexSpecialN + power; num++)
+                {
+                    if (num >= 0 && num < nums.Count)
+                    {
+                        nums[num] = 0;
+                    }
+                }
+            }
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
---
+++ SoftUni C# Fundamentals/7. Lists Exercises/05. Bomb Numbers 1/05. Bomb Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Bomb_Numbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> commands = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == commands[0])
+                {
+                    for (int j = 0; j < commands[1]; j++)
+                    {
+                        if (i + commands[1] <= numbers.Count-1)
+                        {
+                            numbers.RemoveAt(i + 1);
+                        }
+                        else if (i + commands[1] > numbers.Count-1 && i != numbers.Count - 1&&i+1+j>1)
+                        {
+                            numbers.RemoveAt(i + 1);
+                        }
+                        if (i + 1 >= commands[1]&&i>0)
+                        {
+                            numbers.RemoveAt(i - 1);
+                        }
+                        else if (i + 1 < commands[1] && i != 0&&i>=0)
+                        {
+                            numbers.RemoveAt(i - 1);
+                        }                        
+                        i--;// moves Bomb number's index to the left
+                        if (numbers.Count == 1)
+                        {
+                            break;
+                        }
+                    }
+                    i = numbers.IndexOf(commands[0]);//finds the Bomb number and removes it.
+                    numbers.RemoveAt(i);                    
+                }
+            }
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}   
+
