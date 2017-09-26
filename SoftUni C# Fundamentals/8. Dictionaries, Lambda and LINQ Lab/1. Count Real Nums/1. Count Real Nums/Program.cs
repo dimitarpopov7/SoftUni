99d70760fe46a83f6f235413ba70a6bcf0cd444f
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-09-26 11:52:44 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/8. Dictionaries, Lambda and LINQ Lab/1. Count Real Nums/1. Count Real Nums/Program.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _1.Count_Real_Nums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers=Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+            var result = new SortedDictionary<double,int>();
+            foreach (var num in numbers)
+            {                
+                if (!result.ContainsKey(num))
+                {
+                    result[num] = 0;
+                }                
+                result[num]++;
+            }
+            foreach (var number in result)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value} times");
+            }
+        }
+    }
+}
---
+++ SoftUni C# Fundamentals/8. Dictionaries, Lambda and LINQ Lab/2. Odd Occurrences/2. Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2.Odd_Occurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine()
+                .ToLower()
+                .Split(' ')
+                .ToList();
+            var wordCount = new Dictionary<string,int>();
+            foreach (var word in text)
+            {
+                if (!wordCount.ContainsKey(word))
+                {
+                    wordCount[word] = 0;
+                }
+                wordCount[word]++;
+            }
+            var result = new List<string>();
+            foreach (var word in wordCount)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    result.Add(word.Key);
+                }
+            }            
+            Console.WriteLine(string.Join(", ",result)); 
+        }
+    }
+}
