f50a9a38067516bf25038f2958d2a1f3015b1e8f
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-09-29 11:39:35 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/9. Dictionaries, Lambda and LINQ Exercises/02. Phonebook Upgrade/2. Phonebook Upgrade/KeyValuePair.cs
@@ -0,0 +1,6 @@
+﻿namespace _02.Phonebook_Update
+{
+    internal class KeyValuePair<T>
+    {
+    }
+}
---
+++ SoftUni C# Fundamentals/9. Dictionaries, Lambda and LINQ Exercises/02. Phonebook Upgrade/2. Phonebook Upgrade/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _02.Phonebook_Update
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var phonebook = new SortedDictionary<string, string>();
+            while (true)
+            {
+                var entry = Console.ReadLine()
+                    .Split(' ')
+                    .ToList();
+                if (entry[0] == "A")
+                {
+                    phonebook[$"{entry[1]}"] = ($"{entry[2]}");
+                }
+                else if (entry[0] == "ListAll")
+                {
+                    foreach (var item in phonebook)
+                    {
+                        Console.WriteLine($"{item.Key} -> {item.Value}");
+                    }
+                }
+                else if (entry[0] == "S")
+                {
+                    if (phonebook.ContainsKey(entry[1]))
+                    {
+                        Console.WriteLine($"{entry[1]} -> {phonebook[entry[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {entry[1]} does not exist.");
+                    }
+                }
+                else if (entry[0] == "END")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
