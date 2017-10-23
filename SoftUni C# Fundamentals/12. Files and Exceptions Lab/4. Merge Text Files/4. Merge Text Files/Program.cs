0d6d74fba4b7ce6e7d7a9b9108e7ef3861902ebf
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-10-23 17:58:50 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/12. Files and Exceptions Lab/4. Merge Text Files/4. Merge Text Files/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _4.Merge_Text_Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.IO;
+    public class Program
+    {
+        public static void Main()
+        {
+            var listText1 = File.ReadAllText("./TextFile1.txt")
+                .Split(new char[] { ' ','\r','\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var listText2 = File.ReadAllText("./TextFile2.txt")
+                .Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> combinedListTexts = listText1.Concat(listText2)
+                .OrderBy(x=>x)
+                .ToList();
+            File.WriteAllLines("./Output.txt", combinedListTexts);
+        }
+    }
+}
