001e1f430dac1415de8e65604aabf13228f08939
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-10-24 10:27:01 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/12. Files and Exceptions Lab/5. Folder Size/5. Folder Size/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _5.Folder_Size
+{
+    using System;
+    using System.Linq;
+    using System.IO;
+    public class Program
+    {
+        public static void Main()
+        {
+            var files = Directory
+                .GetFiles("FindMySize")
+                .Select(f => new FileInfo(f))//for each file create FileInfo
+                .Sum(f => f.Length);//for each file sum the lenght
+            Console.WriteLine(files/1024/1024);
+        }
+    }
+}
---
+++ SoftUni C# Fundamentals/12. Files and Exceptions Lab/ExceptionsLab/ExceptionsLab/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ExceptionsLab
+{
+    using System;
+    using System.IO;
+    public class Program
+    {
+        public static void Main()
+        {
+            string name = "pesho";
+            int age = 7;
+            
+            try
+            {                 
+                int sum = age + int.Parse(name);
+            }
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine("index out of range");
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (Exception type)
+            //{
+            //    Console.WriteLine("Sorry soemthing went wrong:");
+            //    Console.WriteLine(type.Message);
+            //    Console.WriteLine(type.StackTrace);
+            //}
+            
+
+            //Save exceptions in a log so that the user does not see them
+            catch (Exception ex)
+            {
+                string exceptionLog = "./ErrorLog.txt";
+                File.AppendAllText(exceptionLog, ex.Message);
+                File.AppendAllText(exceptionLog, Environment.NewLine);
+                File.AppendAllText(exceptionLog, ex.StackTrace);
+                File.AppendAllText(exceptionLog, Environment.NewLine);
+                File.AppendAllText(exceptionLog, Convert.ToString(DateTime.Now));
+                File.AppendAllText(exceptionLog, Environment.NewLine);
+                File.AppendAllText(exceptionLog, new string('-', 20));
+            }
+            finally
+            {
+                Console.WriteLine("Final");
+            }            
+        }
+    }
+}
