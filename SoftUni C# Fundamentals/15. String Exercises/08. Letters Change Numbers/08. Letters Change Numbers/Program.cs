ca9c89f684879699ef4c94cf46d7914b6723c7f0
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-10-28 12:18:17 +0100

    SoftUni

---
+++ SoftUni C# Fundamentals/15. String Exercises/08. Letters Change Numbers/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _08.Letters_Change_Numbers
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var entry = Console.ReadLine()
+                .Split(new string[] {" ","\t" },StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            double result = 0;
+            try
+            {
+                foreach (string item in entry)
+                {
+                    double numberManipulation = 0.0;
+                    double number = Convert.ToDouble(item.Substring(1, item.Length - 2));
+                    var currentString = item.ToCharArray();
+                    if (char.IsLower(currentString[0]))
+                    {
+                        int charIndex = currentString[0] - 96;
+                        numberManipulation = number * charIndex;
+                    }
+                    else
+                    {
+                        int charIndex = currentString[0] - 64;
+                        numberManipulation = number / charIndex;
+                    }
+                    int indexOfLastChar = currentString.Length - 1;
+                    if (char.IsLower(currentString[indexOfLastChar]))
+                    {
+                        int charIndex = currentString[indexOfLastChar] - 96;
+                        numberManipulation = numberManipulation + charIndex;
+                    }
+                    else
+                    {
+                        int charIndex = currentString[indexOfLastChar] - 64;
+                        numberManipulation = numberManipulation - charIndex;
+                    }
+                    result += numberManipulation;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"{result:f2}");
+                return;
+            }            
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
---
+++ SoftUni C# Fundamentals/15. String Exercises/09. Melrah Shake/09. Melrah Shake/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _09.Melrah_Shake
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            string entry = Console.ReadLine();
+            string removePattern = Console.ReadLine(); //take out the first and the last occurance of the pattern from the entry 
+            while (entry.Contains(removePattern)&&removePattern.Length>0)
+            {
+                int indexFirst = entry.IndexOf(removePattern);
+                int indexLast = entry.LastIndexOf(removePattern);
+                Console.WriteLine("Shaked it.");
+                entry = entry.Remove(indexFirst, removePattern.Length);
+                entry = entry.Remove(indexLast-removePattern.Length, removePattern.Length);
+                removePattern = removePattern.Remove(removePattern.Length / 2,1);
+            }
+            Console.WriteLine("No shake.");
+            Console.WriteLine(entry);
+        }
+    }
+}
