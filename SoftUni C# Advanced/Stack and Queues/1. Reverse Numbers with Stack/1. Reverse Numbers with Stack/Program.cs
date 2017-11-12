05e0de088113ab928e98962b298775db9b39891b
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-12 00:32:11 +0000

    SoftUni

---
+++ SoftUni C# Advanced/Stack and Queues/1. Reverse Numbers with Stack/1. Reverse Numbers with Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1.Reverse_Numbers_with_Stack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                var numbers = Console.ReadLine()
+                    .Split(new string[] { " " },StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToList();
+                var reversedNumbers = new Stack<int>();
+                numbers.ForEach(x => reversedNumbers.Push(x));//Something new :)
+                Console.WriteLine(string.Join(" ", reversedNumbers));
+            }
+            catch (Exception)
+            {
+                return;
+            }   
+        }
+    }
+}
