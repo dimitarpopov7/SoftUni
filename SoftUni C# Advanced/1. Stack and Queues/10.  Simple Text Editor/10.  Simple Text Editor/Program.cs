8fcd9339d00b7c560711cd4b05f893be2f1d442a
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-14 22:52:59 +0000

    SoftUni

---
+++ SoftUni C# Advanced/1. Stack and Queues/10.  Simple Text Editor/10.  Simple Text Editor/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _10.Simple_Text_Editor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string start = string.Empty;
+            var stack = new Stack<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var command = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                int option = int.Parse(command[0]);
+                //option 1 
+                if (option == 1)
+                {
+                    stack.Push(start);
+                    if (start.Length == 0)
+                    {
+                        start = command[1];
+                    }
+                    else
+                    {
+                        start=start+command[1];
+                    }
+                }
+                //option 2
+                else if (option == 2)
+                {
+                    stack.Push(start);
+                    if (start.Length == 0||start.Length<int.Parse(command[1]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var newString = start.Substring(0, start.Length - int.Parse(command[1]));
+                        start = newString;
+                    }
+                }
+                // option 3 prints
+                else if (option==3)
+                {
+                    try
+                    {
+                        Console.WriteLine(start[int.Parse(command[1]) - 1]);
+                    }
+                    catch (Exception)
+                    {
+
+                        continue;
+                    }
+                }
+                // option 4 reverses previous option 1 or 2
+                else if (option==4)
+                {                    
+                    start = stack.Pop();
+                }
+            }
+        }
+    }
+}
---
+++ SoftUni C# Advanced/1. Stack and Queues/11. Poisonous Plants 2/11. Poisonous Plants 2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _11.Poisonous_Plants_2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var plants = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToList();
+            var indexStack = new Stack<int>();
+            int days = 0;
+            int plantsCount = plants.Count;
+            int newPlantsCount = 0;
+            while (plantsCount!=newPlantsCount)
+            {
+                for (int i = 0; i < plants.Count - 1; i++)
+                {
+                    int left = plants[i];
+                    int right = plants[i+1];
+                    if (left < right)
+                    {
+                        indexStack.Push(i + 1);
+                    }
+                }
+                plantsCount = plants.Count;
+                if (indexStack.Count == 0)
+                {
+                    Console.WriteLine(days);
+                    return;
+                }
+                foreach (var index in indexStack)
+                {
+                    plants.RemoveAt(index);
+                }
+                indexStack.Clear();
+                days++;
+                newPlantsCount = plants.Count;
+            }
+            Console.WriteLine(days);
+        }
+    }
+}
