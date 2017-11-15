0b8dd2dfdbc7707e462cc3fb7f006c2695ad00ad
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-15 09:36:38 +0000

    SoftUni

---
+++ SoftUni C# Advanced/2. Sets and Dictionaries/1. Unique Usernames/1. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1.Unique_Usernames
+{
+    using System;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var uniqueUsernames = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueUsernames.Add(name);
+            }
+            foreach (var username in uniqueUsernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
