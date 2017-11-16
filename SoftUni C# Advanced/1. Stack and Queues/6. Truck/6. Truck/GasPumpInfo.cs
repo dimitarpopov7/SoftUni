9577421db58ae42eaded3e89ea015ea1ed12bc2a
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-16 11:33:05 +0000

    SoftUni

--- SoftUni C# Advanced/1. Stack and Queues/6. Truck/6. Truck/GasPumpInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _6.Truck
-{
-    public class GasPumpInfo
-    {
-        public int FuelAdded { get; set; }
-        public int DistanceToNextStation { get; set; }
-        public int GasPumpIndex { get; set; }
-    }
-}
