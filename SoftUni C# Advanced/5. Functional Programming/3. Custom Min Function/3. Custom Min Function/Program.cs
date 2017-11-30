134db58ae721566d2c42a91e12f486a4805144b4
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-30 02:10:32 +0000

    SoftUni

---
+++ SoftUni C# Advanced/5. Functional Programming/3. Custom Min Function/3. Custom Min Function/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _3.Custom_Min_Function
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            Func<List<int>, int> minNum = GetMinNum;
+            Console.WriteLine(minNum(numbers));
+        }
+
+        private static int GetMinNum(List<int> listOfNums)
+        {
+            return listOfNums.Min();
+        }
+    }
+}
