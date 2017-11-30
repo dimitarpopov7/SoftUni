4b5ec601e09e4e6c6057e97267876b1164d2411b
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-30 02:14:07 +0000

    SoftUni

---
+++ SoftUni C# Advanced/5. Functional Programming/1. Action Print/1. Action Print/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _1.Action_Print
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        static void Main()
+        {
+            var names = Console.ReadLine();
+            Action<string> namePrint =PrintNames;
+            namePrint(names);
+            //with Linq
+        //    Action<string> NamePrint = x => names.Split(' ').ToList().ForEach(c => Console.WriteLine(c));
+        //    NamePrint(names);
+        }
+        public static void PrintNames(string names)
+        {
+            var listNames = names.Split(' ').ToList();
+            foreach (var name in listNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ SoftUni C# Advanced/5. Functional Programming/8. Custom Comparator/8. Custom Comparator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _8.Custom_Comparator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Action<List<int>> comparator = Actions;
+            comparator(numbers);
+        }
+
+        private static void Actions(List<int> numbers)
+        {
+            var evenNums = new List<int>(numbers.Where(x => x % 2 == 0));
+            var oddNums = new List<int>(numbers.Where(x => x % 2 != 0));
+            evenNums.Sort();
+            oddNums.Sort();
+            var newList = evenNums.Concat(oddNums);
+            Console.WriteLine(string.Join(" ",newList));
+        }
+    }
+}
