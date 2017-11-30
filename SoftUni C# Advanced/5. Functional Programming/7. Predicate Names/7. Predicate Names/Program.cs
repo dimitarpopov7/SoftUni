53859bcde36d02cd4930992d61b60eeccdb0d990
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-11-30 16:09:02 +0000

    SoftUni

---
+++ SoftUni C# Advanced/5. Functional Programming/7. Predicate Names/7. Predicate Names/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _7.Predicate_Names
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var allowedLenght = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split().ToList();
+            Predicate<string> meetsRequirement=x=>x.Length<=allowedLenght;
+            var namesLeft = new List<string>();
+            foreach (var name in names)
+            {
+                if (meetsRequirement(name))
+                {
+                    namesLeft.Add(name);
+                }
+            }
+            foreach(var name in namesLeft)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
