94e2b3d0dfc6b97a281ce7973947cfe92f64e785
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-12-01 00:24:53 +0000

    SoftUni

---
+++ SoftUni C# Advanced/6. Built-In Query Methods-LINQ/1. Students By Groups/1. Students By Groups/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1.Students_By_Groups
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var student = Console.ReadLine().Split().ToList();
+            var studentList = new List<Student>();
+            while (student[0]!="END")
+            {
+                var currentStuden = new Student() { Name = student[0],
+                    Surname = student[1],
+                    Group = int.Parse(student[2])};
+                studentList.Add(currentStuden);
+                student = Console.ReadLine().Split().ToList();
+            }
+            foreach (var studen in studentList.Where(x=>x.Group==2).OrderBy(x=>x.Name).ThenBy(x=>x.Surname))
+            {
+                Console.WriteLine($"{studen.Name} {studen.Surname}");
+            }            
+        }
+    }
+}
