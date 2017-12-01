4e1b10b944c035b469568e61a1c5d41e0944eaef
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-12-01 00:28:29 +0000

    SoftUni

---
+++ SoftUni C# Advanced/6. Built-In Query Methods-LINQ/1. Students By Groups/1. Students By Groups/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace _1.Students_By_Groups
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Group { get; set; }
+    }
+}
