b702cc3d38729178df7af9f55e7a2c65259011d4
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-12-15 01:10:44 +0000

    SoftUni

---
+++ SoftUni C# Advanced/3. Matrixes Exercises/1. Matrix of Palindromes/1. Matrix of Palindromes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _1.Matrix_of_Palindromes
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var dimensions = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var alphabet= "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            var matrix = new string[dimensions[0],dimensions[1]];
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                for (int j = 0; j < dimensions[1]; j++)
+                {
+                    string text = $"{alphabet[i]}" + $"{alphabet[i + j]}" + $"{alphabet[i]}";
+                    Console.Write($"{text} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SoftUni C# Advanced/3. Matrixes Exercises/2. Diagonal Difference/2. Diagonal Difference/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _2.Diagonal_Difference
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var matrix = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new double[n];
+                var firstLine = Console.ReadLine().Split().Select(double.Parse).ToList();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = firstLine[j];
+                }
+            }
+            double diagonalOne = 0;
+            double diagonalTwo = 0;
+            int row = 0;
+            int col = 0;
+            int row2 = n-1;
+            int col2 = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                diagonalOne += matrix[row][col];
+                row++;
+                col++;
+                diagonalTwo += matrix[row2][col2];
+                row2--;
+                col2++;
+            }
+            Console.WriteLine(Math.Abs(diagonalOne-diagonalTwo));
+        }
+    }
+}
