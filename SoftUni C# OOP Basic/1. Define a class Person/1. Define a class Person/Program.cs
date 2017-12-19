5825c0b078a9a71099f8250c8a0be9c757080940
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-12-19 12:16:13 +0000

    SoftUni

---
+++ SoftUni C# OOP Basic/1. Define a class Person/1. Define a class Person/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1.Define_a_Class_Person
+{
+    using System;
+    using System.Reflection;
+    public class Define_class_Person
+    {
+        public static void Main()
+        {
+            var pesho = new Person("Pesho", 20);
+            var gosho = new Person("Gosho", 18);
+            var stamat = new Person("stamat", 43);
+
+            Type personType = typeof(Person);
+            PropertyInfo[] fields = personType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+        }
+    }
+}
