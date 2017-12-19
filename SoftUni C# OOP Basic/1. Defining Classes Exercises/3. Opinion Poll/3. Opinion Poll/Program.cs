99bbd3f1a080c3f65b31236529cab99b7016b6f0
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-12-19 14:02:31 +0000

    SoftUni

---
+++ SoftUni C# OOP Basic/1. Defining Classes Exercises/3. Opinion Poll/3. Opinion Poll/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _3.Opinion_Poll
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var listPeople = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                var currentPerson = Console.ReadLine().Split().ToList();
+                var person = new Person(currentPerson[0], int.Parse(currentPerson[1]));
+                listPeople.Add(person);
+            }
+            foreach (var guy in listPeople.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{guy.Name} - {guy.Age}");
+            }
+        }
+    }
+}
