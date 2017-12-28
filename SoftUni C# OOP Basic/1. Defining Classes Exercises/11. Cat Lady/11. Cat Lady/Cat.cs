0a3ef0d56f23e8dd681598f34970b9d287e7bd2b
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2017-12-28 13:14:18 +0000

    SoftUni

---
+++ SoftUni C# OOP Basic/1. Defining Classes Exercises/11. Cat Lady/11. Cat Lady/Cat.cs
@@ -0,0 +1,61 @@
+﻿namespace _11.Cat_Lady
+{
+    public class Cat
+    {
+        private string type;
+        public string Type
+        {
+            get { return this.type; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+        }
+        private int dicibels;
+        public int Dicibels
+        {
+            get { return this.dicibels ; }
+            set { this.dicibels = value; }
+        }
+
+        private double furLenght;
+        public double FurLenght
+        {
+            get { return this.furLenght; }
+            set { this.furLenght = value; }
+        }
+
+        private int earSize;
+        public int EarSize
+        {
+            get { return this.earSize; }
+            set { this.earSize = value; }
+        }
+
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+
+        public Cat(string name, string type) : this(name)
+        {
+            this.type = type;
+        }
+
+        public Cat(string type, string name, int dicibels) : this(name, type)
+        {
+            this.dicibels = dicibels;
+        }
+
+        public Cat(string type, int earSize, string name) : this(name, type)
+        {
+            this.earSize = earSize;
+        }
+        public Cat(string type, string name, double furLenght) : this(name, type)
+        {
+            this.furLenght = furLenght;
+        }
+    }
+}
---
+++ SoftUni C# OOP Basic/1. Defining Classes Exercises/11. Cat Lady/11. Cat Lady/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _11.Cat_Lady
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var info = Console.ReadLine();
+            var cats = new List<Cat>();
+            while (info != "End")
+            {
+                var catInfo = info.Split().ToList();
+                var type = catInfo[0];
+                var currentCat = new Cat(catInfo[1],type);
+
+                if (type == "StreetExtraordinaire")
+                {
+                    currentCat.Dicibels = int.Parse(catInfo[2]);
+                }
+
+                else if (type == "Siamese")
+                {
+                    currentCat.EarSize = int.Parse(catInfo[2]);
+                }
+                else if (type == "Cymric")
+                {
+                    currentCat.FurLenght = double.Parse(catInfo[2]);
+                }
+                cats.Add(currentCat);
+                info = Console.ReadLine();
+            }
+            var searchCat = Console.ReadLine();
+            var neededCat = cats.FirstOrDefault(x => x.Name == searchCat);
+            Console.Write($"{neededCat.Type} {neededCat.Name} ");
+            switch (neededCat.Type)
+            {
+                case "StreetExtraordinaire":
+                    Console.WriteLine(neededCat.Dicibels);
+                    break;
+                case "Cymric":
+                    Console.WriteLine($"{ neededCat.FurLenght:f2}");
+                    break;
+                case "Siamese":
+                    Console.WriteLine(neededCat.EarSize);
+                    break;
+            }
+        }
+    }
+}
