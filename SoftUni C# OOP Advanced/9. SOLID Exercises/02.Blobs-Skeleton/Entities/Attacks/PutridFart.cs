a27dd271e0ac87f503c0e581934944fd9a6b942c
dimitarpopov7 <31646355+dimitarpopov7@users.noreply.github.com>
2018-03-22 11:29:30 +0000

    SoftUni

---
+++ SoftUni C# OOP Advanced/9. SOLID Exercises/02.Blobs-Skeleton/Entities/Attacks/PutridFart.cs
@@ -0,0 +1,8 @@
+﻿namespace _02.Blobs.Entities.Attacks
+{
+    using Interfaces;
+    public class PutridFart : IAttack
+    {
+        public void Execute(IBlob attacker, IBlob target) => target.TakeAttack(attacker.Damage);
+    }
+}
